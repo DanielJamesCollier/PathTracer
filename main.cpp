7001bf187b6c6d939fffbec20fd9d75a144869a6
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-01-25 18:42:04 +0000

    add Trace diagonostics
    add percented traced console output
    add Trace info to console output. prints when trace is complete

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <climits>
 #include <cmath>
+#include <chrono>
+#include <vector>
 
 #include "Vec3.hpp"
 #include "Ray.hpp"
@@ -35,8 +38,7 @@ float raySphereIntersect(Sphere const & sphere, Ray const & ray) {
 
 //---------------------------------------------------------
 Maths::Vec3 backgroundColour(Ray const & r) {
-    Maths::Vec3 unitDircetion = r.direction();
-    unitDircetion.normalise();
+    Maths::Vec3 unitDircetion = Maths::normalise(r.direction());
     
     float t = 0.5f * (unitDircetion.getY() + 1.0f);
     
@@ -45,10 +47,7 @@ Maths::Vec3 backgroundColour(Ray const & r) {
 
 //---------------------------------------------------------
 Maths::Vec3 sphereColour(Ray const & r, float t) {
-    Maths::Vec3 N = r.pointAtParam(t) - Maths::Vec3(0.0f,0.0f,-1.0f);
-    
-    N.normalise();
-    
+    Maths::Vec3 N = Maths::normalise(r.pointAtParam(t) - Maths::Vec3(0.0f,0.0f,-1.0f));
     return 0.5f * Maths::Vec3(N.getX() + 1.0f, N.getY() + 1.0f, N.getZ() + 1.0f);
 }
 
@@ -56,27 +55,67 @@ Maths::Vec3 sphereColour(Ray const & r, float t) {
 int main(int argc, const char * argv[]) {
     int width  {2000};
     int height {1000};
-    std::string outputLocation{"/Users/danielcollier/Desktop/path tracer output.ppm"};
-    
+    std::string outputLocation{"build\\bin\\output\\render.ppm"};
+
     std::ofstream file;
     file.open(outputLocation);
-    file << "P3\n" << width << " " << height << "\n255\n";
-    
+
+    if(!file.is_open()) {
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "File failed to open" << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        std::cerr << "file: " << outputLocation << std::endl;
+        std::cout << "----------------------------------------" << std::endl;
+        return -1;
+    }
+
+    std::cout << "\n\n----------------------------------------" << std::endl;
+    std::cout << "Trace Started" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    file << "P3\n" << width << " " << height << "\n255\n";    
+
+    typedef std::chrono::system_clock Clock;
+    typedef std::chrono::milliseconds milli;
+
+    int raysPerSecond       = 0;
+    int maxRPS              = 0;
+    int minRPS              = INT_MAX;
+    int percentRendered     = 0;
+    int onePercent          = (width * height) / 100;
+    int currentItt          = 0;
+    auto startRender        = Clock::now();
+    auto startSecondCounter = Clock::now();
     Camera cam(Maths::Vec3(0,0,0));
-    
-    Sphere sphere(Maths::Vec3(0,0,-1), 0.5);
-    Sphere sphereTwo(Maths::Vec3(0,-100.5,-1), 100);
+    Sphere sphere(Maths::Vec3(0,0,-1),0.5f);
 
     for(int j {height - 1}; j >= 0; j--) {
         for(int i{0}; i < width; i++) {
+            auto currentTime = Clock::now();
+            auto duration    = std::chrono::duration_cast<milli>(currentTime - startSecondCounter);
+            raysPerSecond++;
+            currentItt++;
+
+            // calculate percent rendered
+            if(currentItt > onePercent) {
+                percentRendered++;
+                currentItt = 0;
+            }
+
+            // calculate RPS
+            if(duration > milli(1000)) {
+                std::cout << "tracing..." << percentRendered << "%" << " - RPS: " << raysPerSecond << std::endl;
+                if(raysPerSecond > maxRPS) maxRPS = raysPerSecond;
+                if(raysPerSecond < minRPS) minRPS = raysPerSecond;
+                raysPerSecond = 0;
+                startSecondCounter = Clock::now();
+            }
             
-            float u = float(i  ) / float(width);
-            float v = float(j ) / float(height);
+            float u = (float)(i) / (float)(width);
+            float v = (float)(j) / (float)(height);
             
             Ray ray = cam.getRay(u, v);
             Maths::Vec3 col;
         
-            
             float t;
             if((t = raySphereIntersect(sphere, ray)) > 0.0f) {
                 col = sphereColour(ray, t);
@@ -92,7 +131,19 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    file.close();
+    auto endRender = std::chrono::system_clock::now();
+    std::cout << "----------------------------------------" << std::endl;
     
+    file.close();
+
+    std::cout << "\n\n----------------------------------------" << std::endl;
+    std::cout << "Trace Info" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << "Trace time [in milliseconds]: " << std::chrono::duration_cast<std::chrono::milliseconds>(endRender - startRender).count() << "ms"<< std::endl;
+    std::cout << "Trace time [in seconds]:      " << std::chrono::duration_cast<std::chrono::seconds>(endRender - startRender).count() << "s"<< std::endl;
+    std::cout << "Max RPS [rays per second]:    " << maxRPS << std::endl;
+    std::cout << "Min RPS [rays per second]:    " << minRPS << std::endl;
+    std::cout << "Total Rays:                   " << width * height << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
     return 0;
 }
